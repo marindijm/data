410001ac907b9f74e6641d74d9faee920e33df07
M <A@B.COM>
2012-12-11 17:19:04 +0300

    Create Issue With Popup

--- dispenserapp/protected/views/issues/_create.php
+++ dispenserapp/protected/views/issues/_create.php
@@ -13,11 +13,88 @@
 
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
 
+<link href="css/jquery-ui-1.9.2.custom.min.css" rel="stylesheet"> 
+<link href="css/ui.jqgrid.css" rel="stylesheet">
+<script src="js_funcs/jquery-1.8.2.min.js" type="text/javascript"></script>
+<script src="js_funcs/jquery-ui-1.9.2.custom.min.js" type="text/javascript"></script>
+<script src="js_funcs/jquery.jqGrid.min.js" type="text/javascript"></script>
+<script src="js_funcs/grid.locale-en.js" type="text/javascript"></script> 
+<script type="text/javascript"> 
+$(document).ready(function() { 
+$("#list1").jqGrid({
+    url:'server.php',
+	datatype: "json",
+	 width:900,
+    height:250,
+   	colNames:['Waterpoint ID','Waterpoint Name','Program Code','District Name','Division Name','Location Name'],
+   	colModel:[
+   		{name:'wptid',index:'wptid', width:45, sorttype:'int'},
+   		{name:'wptname',index:'wptname', width:90},
+   		{name:'pcode',index:'pcode', width:50},
+   		{name:'dname',index:'dname', width:80, align:"right"},
+   		{name:'dvname',index:'dvname', width:80, align:"right"},		
+   		{name:'lname',index:'lname', width:80,align:"right"}	
+   	],
+    rowNum:10,
+	rowTotal: 2141,
+	rowList : [10,20,30],
+	loadonce:true,
+   	mtype: "GET",
+	rownumbers: true,
+	rownumWidth: 40,  
+	gridview: true,
+	pager: '#pager1',
+   	sortname: 'wptid',	
+    viewrecords: true,
+    sortorder: "asc",
+    caption:"DSW Waterpoints"
+});
+$("#list1").jqGrid('navGrid','#pager1',{del:false,add:false,edit:false});
+
+$("#a1").click( function(){
+	var id = $("#list1").jqGrid('getGridParam','selrow');
+	if (id)	{
+		var ret = $("#list1").jqGrid('getRowData',id);
+	//	alert("watrepoint id="+ret.wptid+" waterpoint name="+ret.wptname+"...");
+	$('#wptid').val(ret.wptid);
+	$('#dialog').dialog('close');
+		 return false; 
+	} else 
+	{// alert("Please select row");}
+	
+	}
+	
+	 
+});
+$('div#dialog').dialog({ autoOpen: false })
+	 $('a#dialog_link').click(function() { 
+	  $('#dialog').dialog( "option", "width", 920 );
+		  $('#dialog').dialog('open'); 
+		  return false; 
+	  });
+
+});
+
+			
+</script> 
+
+
+ <div id="dialog" title="Select Waterpoint" style="display:none"> 
+	 <table id="list1"></table>
+	<div id="pager1"></div>
+	<a href="" id="a1">Get Selected Waterpoint</a>
+ </div>  
+   <a href="#" id="dialog_link">Select Waterpoint</a>  
+
 	<?php echo $form->errorSummary($model); ?>
+	
+
+
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'waterpointid'); ?>
-		<?php echo $form->dropDownList($model,'waterpointid', CHtml::listData(Waterpoint::model()->findAll(), 'waterpoint_id', 'waterpoint_name')); ?>
+	
+		<?php echo $form->textField($model,'waterpointid',array('readonly'=>true,'id'=>'wptid')); ?>
 		<?php echo $form->error($model,'waterpointid'); ?>
 	</div>
 
---
+++ dispenserapp/server.php
@@ -0,0 +1,85 @@
+<?php
+$page = @$_GET['page']; // get the requested page
+$limit = @$_GET['rows']; // get how many rows we want to have into the grid
+$sidx = @$_GET['sidx']; // get index row - i.e. user click to sort
+$sord = @$_GET['sord']; // get the direction
+if(!$sidx) $sidx = 1;
+
+$totalrows = isset($_REQUEST['totalrows']) ? $_REQUEST['totalrows']: false;
+if($totalrows) {
+	$limit = $totalrows;
+}
+
+// connect to the database
+ $dbc = pg_connect("host=127.0.0.1 port=5432 dbname=dsw_db user=postgres password=postgres");
+		 if (!$dbc) {
+			 die("Error in connection: " . pg_last_error());
+		 }
+
+
+$result = pg_query($dbc,"SELECT COUNT(*) as cnt FROM   waterpoint_summary ");
+$row = pg_fetch_array($result,NULL, PGSQL_ASSOC);
+$count = $row['cnt'];
+if( $count >0 ) {
+	$total_pages = ceil($count/$limit);
+} else {
+	$total_pages = 0;
+}
+if ($page > $total_pages) $page=$total_pages;
+if ($limit<0) $limit = 0;
+$start = $limit*$page - $limit; // do not put $limit*($page - 1)
+if ($start<0) $start = 0;
+$SQL = 'SELECT "Waterpoint ID" as wptid, "Waterpoint Name" as wptname, "Program Code" as pcode, "District Name" as dname, 
+       "Division Name" as dvname, "Location Name" as lname
+  FROM waterpoint_summary
+ ORDER BY '.$sidx.'  '. $sord .' LIMIT  '.$limit.' OFFSET '.$start.';';
+		//die($SQL );		
+$result =  pg_query($dbc, $SQL );
+
+
+@$responce->page = $page;
+$responce->total = $total_pages;
+$responce->records = $count;
+$i=0;
+while($row = pg_fetch_array($result,NULL, PGSQL_ASSOC)) {
+    $responce->rows[$i]['id']=$row['wptid'];
+    $responce->rows[$i]['cell']=array($row['wptid'],$row['wptname'],$row['pcode'],
+	$row['dname'],$row['dvname'],$row['lname'],"");
+    $i++;
+} 
+pg_close($dbc);
+
+header("Content-Type:application/json");     
+echo json_encode($responce);
+function Strip($value)
+{
+	if(get_magic_quotes_gpc() != 0)
+  	{
+    	if(is_array($value))  
+			if ( array_is_associative($value) )
+			{
+				foreach( $value as $k=>$v)
+					$tmp_val[$k] = stripslashes($v);
+				$value = $tmp_val; 
+			}				
+			else  
+				for($j = 0; $j < sizeof($value); $j++)
+        			$value[$j] = stripslashes($value[$j]);
+		else
+			$value = stripslashes($value);
+	}
+	return $value;
+}
+function array_is_associative ($array)
+{
+    if ( is_array($array) && ! empty($array) )
+    {
+        for ( $iterator = count($array) - 1; $iterator; $iterator-- )
+        {
+            if ( ! array_key_exists($iterator, $array) ) { return true; }
+        }
+        return ! array_key_exists(0, $array);
+    }
+    return false;
+}
+?>
