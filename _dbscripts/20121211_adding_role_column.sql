0fe0ec7282b433acf4c01c6d252e0f5150e7ed2f
Simeon Obwogo <sobwogo@poverty-action.org>
2012-12-13 14:53:46 +0300

    Creating views
    Created views; issues_all, issues_resolved, issues_not_resolved.
    For use in making the app dashboard

--- _dbscripts/20121211_adding_role_column.sql
+++ _dbscripts/20121211_adding_role_column.sql
@@ -1,1 +1,6 @@
 alter table users add column role character varying(255);
+
+-------------13/12/2012-----------------------------------------
+Created views; issues_all, issues_resolved, issues_not_resolved
+For use in making dashboards
+----------------------------------------------------------------
