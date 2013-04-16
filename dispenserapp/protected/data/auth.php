1326a730e095a36fb4c7b94239b41640f6167c91
dsw-coder <mnzuki@poverty-action.org>
2013-04-16 13:02:02 +0300

    a

---
+++ dispenserapp/protected/data/auth.php
@@ -0,0 +1,46 @@
+<?php
+return array (
+  'reader' => 
+  array (
+    'type' => 2,
+    'description' => 'Only view dispenserapp',
+    'bizRule' => '',
+    'data' => '',
+  ),
+  'writer' => 
+  array (
+    'type' => 2,
+    'description' => 'Create and edit',
+    'bizRule' => '',
+    'data' => '',
+    'children' => 
+    array (
+      0 => 'reader',
+    ),
+  ),
+  'admin' => 
+  array (
+    'type' => 2,
+    'description' => 'Admin',
+    'bizRule' => '',
+    'data' => '',
+    'children' => 
+    array (
+      0 => 'reader',
+      1 => 'writer',
+    ),
+    'assignments' => 
+    array (
+      1000 => 
+      array (
+        'bizRule' => NULL,
+        'data' => NULL,
+      ),
+      1558 => 
+      array (
+        'bizRule' => NULL,
+        'data' => NULL,
+      ),
+    ),
+  ),
+);
